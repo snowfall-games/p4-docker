1ab55883672472202b2f61824f9cca575ea5ef3e
Jaden Holladay <jadenholladay@gmail.com>
2025-08-31 20:33:57 -0700

    another ipv6 config

--- files/setup.sh
+++ files/setup.sh
@@ -10,13 +10,23 @@ mv /etc/perforce /etc/perforce.orig
 ln -s "$P4ROOT/etc" /etc/perforce
 
 if ! p4dctl list 2>/dev/null | grep -q "$NAME"; then
-    /opt/perforce/sbin/configure-helix-p4d.sh "$NAME" -n -p "$P4PORT" -r "$P4ROOT" -u "$P4USER" -P "${P4PASSWD}" --case "$P4CASE" --unicode
+    # Use just the port number for initial configuration
+    /opt/perforce/sbin/configure-helix-p4d.sh "$NAME" -n -p "$P4TCP" -r "$P4ROOT" -u "$P4USER" -P "${P4PASSWD}" --case "$P4CASE" --unicode
 fi
+
+# Start server with initial IPv4 configuration
+p4dctl start -t p4d "$NAME"
+
+# Wait for server to be ready, then reconfigure for IPv6
+sleep 3
+echo "Reconfiguring server for IPv6 binding..."
+P4PORT="$P4TCP" p4 configure set $NAME#P4PORT="$P4PORT"
+
+# Restart server with new IPv6 configuration
+p4dctl restart -t p4d "$NAME"
 
 p4 configure set $P4NAME#server.depot.root=$P4DEPOTS
 p4 configure set $P4NAME#journalPrefix=$P4CKP/$JNL_PREFIX
-
-p4dctl start -t p4d "$NAME"
 
 # Configure Unreal Engine file types per Epic Games documentation
 p4 typemap -i < /usr/local/bin/unreal-typemap.txt
